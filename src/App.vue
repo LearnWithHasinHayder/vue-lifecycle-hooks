23837769b21c0cbe5c56ccc6502a8b2da45b322a
Hasin Hayder <countdraculla@gmail.com>
2023-08-08 09:49:26 +0600

    Carousel

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, onUpdated, onBeforeMount } from 'vue';
+const items = [
+  'https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1467&q=80',  
+  'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',  
+  'https://images.unsplash.com/photo-1558882224-dda166733046?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80',  
+  'https://images.unsplash.com/photo-1562664377-709f2c337eb2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',  
+  'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',  
+  'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80',  
+
+]
+
+onMounted(() => {
+  var elem = document.getElementById('hello');
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+})
+</script>
+
+<template>
+ 
+ <div class="items" id="hello">
+    <div :style="`background-image:url(${item})`" class="item" v-for="item, index in items" :key="item">{{ index+1 }}</div>
+  </div>
+</template>
+
+<style scoped>
+.items {
+  width: 600px;
+  height: 400px;
+}
+
+.item {
+  width: 600px;
+  height: 400px;
+  background-color: #ccc;
+  background-size: cover;
+}
+</style>
